1dfe6c1774d9b070ae8483041fc25b7758b7df8a
geek <1163518793@qq.com>
2019-12-23 15:10:58 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot/managerinititalizr/ManagerInititalizrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.managerinititalizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerInititalizrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerInititalizrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot/managerinititalizr/ManagerInititalizrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.managerinititalizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerInititalizrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
